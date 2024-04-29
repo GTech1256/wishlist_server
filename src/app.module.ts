3bac3121916119246745810c7c5c99bf22a64b05
Roman Bakakin <60231318@leroymerlin.ru>
2024-04-29 19:52:31 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
-import { ListController } from './list/list.controller';
-import { ListService } from './list/list.service';
 import { ListModule } from './list/list.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from './item/entities/item.entity';
 import { User } from './user/entities/user.entity';
 import { List } from './list/entities/list.entity';
-import { ItemController } from './item/item.controller';
-import { UserController } from './user/user.controller';
-import { ItemService } from './item/item.service';
 
 @Module({
   imports: [
@@ -30,7 +25,7 @@ import { ItemService } from './item/item.service';
     ListModule,
     UserModule,
   ],
-  controllers: [AppController, ListController, ItemController, UserController],
-  providers: [AppService, ListService, ItemService, UserController],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
