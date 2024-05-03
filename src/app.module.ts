a4cdb5d00bebb8c5c319cfbe6cd0e4f7b0526d35
Roman Bakakin <pomah156@gmail.com>
2024-05-03 11:07:07 +0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { List } from './list/entities/list.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'db',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
