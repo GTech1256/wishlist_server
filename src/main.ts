340b6b939db55c9cc64cf6cb0ea3616a8845631e
Roman Bakakin <60231318@leroymerlin.ru>
2024-05-16 00:12:17 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // docker-compose тоже указывается порт
 const PORT = 3000;
@@ -7,6 +8,7 @@ const PORT = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   console.log('Service trying start on PORT ' + PORT);
   await app.listen(PORT);
