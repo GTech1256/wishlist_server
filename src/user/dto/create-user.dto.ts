fdff8d03cd6d4ea7faa0d4369fb9e7e2abea880a
Roman Bakakin <60231318@leroymerlin.ru>
2025-02-02 04:38:55 +0300

    test

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsNotEmpty, IsPositive } from 'class-validator';
+import {
+  IsNumber,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
 import { PrimaryColumn } from 'typeorm';
 
 export class CreateUserDto {
@@ -7,18 +13,26 @@ export class CreateUserDto {
   @IsNumber()
   @IsNotEmpty()
   @IsPositive()
-  @PrimaryColumn({ nullable: false, unique: true, type: 'int' })
+  @PrimaryColumn({ nullable: false, unique: true, type: 'int', primary: true })
   id: number;
 
   @ApiProperty({ description: 'Имя пользователя' })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty({ description: 'Фамилия пользователя' })
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 
   @ApiProperty({ description: 'Никнейм пользователя' })
+  @IsString()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty({ description: 'Ссылка на аватар пользователя' })
+  @IsString()
+  @IsOptional()
   avatar?: string;
 }
